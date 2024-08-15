8b5305d8fb91816dd02474f01593926aef24ae0d
mai420 <pappumaithry@gmail.com>
2024-08-15 13:25:56 +0530

    Add an empty tsx file named 'ClickState.tsx'

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomePage from "./components/WelcomePage";
 
+
 export default function App() {
   return (
     <View>
