1628e4742faa68a5125eb6d4ff13bf501704535e
mai420 <pappumaithry@gmail.com>
2024-08-16 01:05:32 +0530

    Changed the component name from ClickState to ClickCorrectState

--- components/ClickCorrectState.tsx
+++ components/ClickCorrectState.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View, Image} from 'react-native';
 
-export default function ClickState() {
+export default function ClickCorrectState() {
     return(
         <View>
             <Text>Click the state</Text>
