14ab10257eee22847126572e6d5e0db802f25d33
mai420 <pappumaithry@gmail.com>
2024-08-16 00:05:45 +0530

    Add the import statement for react and fix EOFs

--- components/ClickState.tsx
+++ components/ClickState.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import {Text, View, Image} from 'react-native';
-
 
 export default function ClickState() {
     return(
         <View>
             <Text>Click the state</Text>
         </View>
-    )
-    
+    )  
 }
