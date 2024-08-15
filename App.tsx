4b43d6d9f5aca3810c6f0f7547c40bb2a9adccfb
mai420 <pappumaithry@gmail.com>
2024-08-15 15:01:23 +0530

    Add ClickState.tsx file in components and import ClickState function in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomePage from "./components/WelcomePage";
+import ClickState from "./components/ClickState";
 
 export default function App() {
   return (
     <View>
       <WelcomePage />
+      <ClickState></ClickState>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/ClickState.tsx
@@ -0,0 +1,3 @@
+export default function ClickState() {
+    return <div>Click on the correct state!</div>
+}
