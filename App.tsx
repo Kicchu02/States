a706a1750f577139488d6c5c4388285a9cb64314
mai420 <pappumaithry@gmail.com>
2024-08-16 01:57:45 +0530

    Remove the import statements of components in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomePage from "./components/WelcomePage";
-import ClickState from "./components/ClickState";
-import EnterStateName from "./components/EnterStateName";
-import StateQuiz from "./components/StateQuiz";
 
 export default function App() {
   return (
     <View>
       <WelcomePage />
-      <ClickState />
-      <EnterStateName/>
-      <StateQuiz />
       <StatusBar style="auto" />
     </View>
   );
