e0faed4bd87cebf4b32e6a1528fdbc4ab015345f
maithry <pappumaithry@gmail.com>
2024-08-17 02:49:53 +0530

    Remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
