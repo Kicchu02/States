af61675d2faca734add785f4d759fd881d02d1fa
maithry <pappumaithry@gmail.com>
2024-08-17 10:19:19 +0530

    Fix imports

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import style from "../styles";
+import style from "../Styles";
 
 export default function WelcomePage() {
   return (
