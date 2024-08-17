209e4e7b40022bbac9ce09bc50ff17e2bb9ecb59
maithry <pappumaithry@gmail.com>
2024-08-17 11:25:25 +0530

    Create StyledButton.tsx

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import style from "../Styles";
+import StyledButton from "../ui-components/StyledButton";
 
 export default function WelcomePage() {
   return (
     <View style={style.container}>
       <Text style={style.textHeaderStyle}>WELCOME TO STATES APP!</Text>
-      <TouchableOpacity style={style.buttonStyle}>
-        <Text style={style.butonTextStyle}>Click on the correct state</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={style.buttonStyle}>
-        <Text style={style.butonTextStyle}>Enter correct state name</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={style.buttonStyle}>
-        <Text style={style.butonTextStyle}>Quiz on states</Text>
-      </TouchableOpacity>
+      <StyledButton text="Select State" />
+      <StyledButton text="Enter State" />
+      <StyledButton text="States Quiz" />
     </View>
   );
 }
