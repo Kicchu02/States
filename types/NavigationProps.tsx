13a0e945777d1719a3ba2e75fbddb63b646c7c07
maithry <pappumaithry@gmail.com>
2024-08-18 15:06:15 +0530

    Define precise screen names

--- types/NavigationProps.tsx
+++ types/NavigationProps.tsx
@@ -3,8 +3,9 @@ import { StackNavigationProp } from "@react-navigation/stack";
 export type StackParamList = {
   WelcomePage: undefined;
   ClickCorrectStatePage: undefined;
-  EnterStatePage: undefined;
-  StatesQuizPage: undefined;
+  EnterCorrectStateNamePage: undefined;
+  StateMultipleChoiceQuizPage: undefined;
 };
 
 export type NavigationProps = StackNavigationProp<StackParamList>;
+export type ScreenNames = keyof StackParamList;
