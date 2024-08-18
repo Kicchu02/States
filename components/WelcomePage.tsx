2f82c377e7d0a1c1324701544a9f734122e148cd
maithry <pappumaithry@gmail.com>
2024-08-18 15:03:50 +0530

    Implement navgation using NavigationProps

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { Text, View } from "react-native";
 import style from "../Styles";
 import StyledButton from "../ui-components/StyledButton";
+import { useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ScreenNames } from "../types/NavigationProps";
 
 export default function WelcomePage() {
+  const navigation = useNavigation();
+  const stack = createStackNavigator();
   return (
     <View>
       <Text style={style.textHeaderStyle}>WELCOME TO STATES APP!</Text>
-      <StyledButton text="Select State" />
-      <StyledButton text="Enter State" />
-      <StyledButton text="States Quiz" />
+      <StyledButton text="Select State" navigateTo="ClickCorrectStatePage" />
+      <StyledButton text="Enter State" navigateTo="EnterCorrectStateNamePage" />
+      <StyledButton
+        text="States Quiz"
+        navigateTo="StateMultipleChoiceQuizPage"
+      />
     </View>
   );
 }
